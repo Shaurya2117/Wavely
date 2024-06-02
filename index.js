4518caa0a5f17710df659c95d018b9b67f94d250
Shaurya <110289166+Shaurya2117@users.noreply.github.com>
2024-06-02 14:14:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+// sidebar
+const menuitems = document.querySelectorA11('.menu-item');
+
+// massages
+const messagesnotificaton = document.querySelector
+('#messages-notifications');
+const messages = document.querySelector('.message');
+
+// ***********sidebar*************
+
+// remove active class from all menu items
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+        
+    })
+}
+
+menuitems.forEach(item => {
+    item.addeventlistener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if(item.id != 'notification'){
+            document.querySelector('.notification-popup').
+            style.disply = 'none';
+        }
+        else{
+            document.querySelector('notification-popup').
+            style.disply = 'block';
+            document.querySelector('#notification .notification-count')
+            .style.disply = 'none';
+        }
+    })
+})
+
+// ***********messages*************
+messagesnotificaton.addEventListener('click', () => {
+    messages.style.boxshadow = '0 0 1rem var(--color-primary)';
+})
+
+
+
+
